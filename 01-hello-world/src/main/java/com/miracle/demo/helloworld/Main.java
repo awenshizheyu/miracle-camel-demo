09011da47c16d8cc9b055473fcd0bffd513145a9
awen <391585925@qq.com>
2019-11-18 16:26:06 +0800

    第一次提交

---
+++ 01-hello-world/src/main/java/com/miracle/demo/helloworld/Main.java
@@ -0,0 +1,25 @@
+/**
+ * Project Name : miracle-camel-demo
+ * File Name    : Main
+ * Package Name : com.miracle.demo.helloworld
+ * Date         : 2019-11-18 16:13
+ * Author       : fan.wenbin
+ * Copyright (c) 2018, fan.wenbin@ustcinfo.com All Rights Reserved.
+ */
+package com.miracle.demo.helloworld;
+
+/**
+ * @program: miracle-camel-demo
+ * @className: Main
+ * @description: TODO
+ * @CreateTime: 2019-11-18 16:13
+ * @author: fan.wenbin
+ * @version: V1.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
